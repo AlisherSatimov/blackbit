b280ecfc618e65fac6221950f9def758281d2989
Alisher Satimov <alishersatimov07@gmail.com>
2026-03-25 12:10:32 +0500

    feat: add >_ SVG favicon with dark/light mode support

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,6 +76,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <script
